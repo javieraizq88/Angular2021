e4064b7830f36582ade9dc2aeee30cf2584d66aa
Javiera Izquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2021-11-30 17:38:36 -0300

    ejercicio 1 y 2

--- ts-intro/src/ejercicios/01-tipos-basicos.ts
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -2,6 +2,9 @@
     ===== Código de TypeScript =====
 */
 
+
+// let habildades: (boolean | string | number)[] = ["Bash", "Counter", "Healing", 100]; 
+
 let nombre: string = 'Strider';
 let hp: number | string = 95;
 let estaVivo: boolean = true;
@@ -10,5 +13,3 @@ hp = 'FULL';
 
 
 console.log( nombre, hp );
-
-
