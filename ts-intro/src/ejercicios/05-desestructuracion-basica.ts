9b3ba39bd3f314ce53a3956e4b22663c98600256
Javiera Izquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2022-01-13 11:57:18 -0300

    desesctructuracion

--- ts-intro/src/ejercicios/05-desestructuracion-basica.ts
+++ ts-intro/src/ejercicios/05-desestructuracion-basica.ts
@@ -1,42 +1,42 @@
-/*
-    ===== Código de TypeScript =====
-*/
+// /*
+//     ===== Código de TypeScript =====
+// */
 
-interface Reproductor {
-    volumen: number;
-    segundo: number;
-    cancion: string;
-    detalles: Detalles 
-}
+// interface Reproductor {
+//     volumen: number;
+//     segundo: number;
+//     cancion: string;
+//     detalles: Detalles 
+// }
 
-interface Detalles {
-    autor: string;
-    anio: number;
-}
+// interface Detalles {
+//     autor: string;
+//     anio: number;
+// }
 
-const reproductor: Reproductor = {
-    volumen: 90,
-    segundo: 36,
-    cancion: 'Mess',
-    detalles: {
-        autor: 'Ed Sheeran',
-        anio: 2015
-    }
-}
+// const reproductor: Reproductor = {
+//     volumen: 90,
+//     segundo: 36,
+//     cancion: 'Mess',
+//     detalles: {
+//         autor: 'Ed Sheeran',
+//         anio: 2015
+//     }
+// }
 
-const { volumen, segundo, cancion, detalles } = reproductor;
-const { autor } = detalles;
-
-
-// console.log('El volumen actual de: ', volumen );
-// console.log('El segundo actual de: ', segundo );
-// console.log('La canción actual de: ', cancion );
-// console.log('El autor es: ', autor );
+// const { volumen, segundo, cancion, detalles } = reproductor;
+// const { autor } = detalles;
 
 
-const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
-const [ , , p3 ] = dbz;
+// // console.log('El volumen actual de: ', volumen );
+// // console.log('El segundo actual de: ', segundo );
+// // console.log('La canción actual de: ', cancion );
+// // console.log('El autor es: ', autor );
 
-// console.log('Personaje 1:', p1 );
-// console.log('Personaje 2:', p2 );
-console.log('Personaje 3:', p3 );
+
+// const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+// const [ , , p3 ] = dbz;
+
+// // console.log('Personaje 1:', p1 );
+// // console.log('Personaje 2:', p2 );
+// console.log('Personaje 3:', p3 );
